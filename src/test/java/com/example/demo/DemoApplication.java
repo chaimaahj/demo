baaaaacb144a2d9edae140ff75b7f27934c901ad
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-03-01 15:10:22 +0100

    Add files via upload

---
+++ src/test/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.model.Produit;
+import com.example.repositories.ProduitRepository;
+
+
+public class DemoApplication {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	
+}
