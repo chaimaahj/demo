36e969c894e823cf3aea132ec58b01c4b501ffc3
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-03-08 14:26:04 +0100

    Add files via upload

--- src/main/java/com/example/repositories/ProduitRepository.java
+++ src/main/java/com/example/repositories/ProduitRepository.java
@@ -1,12 +1,11 @@
 package com.example.repositories;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.model.Produit;
 
 @Repository
-public interface ProduitRepository extends CrudRepository<Produit, Integer>{
+public interface ProduitRepository extends JpaRepository<Produit, Integer>{
 	
 	@Transactional(readOnly = true)
     @Cacheable("produits")
-	public Optional<Produit> findById(@Param("id") Integer id);
-	public Collection<Produit> findAll() throws DataAccessException;
+	public Optional<Produit> findById(int id) throws DataAccessException;
+	public List<Produit> findAll() throws DataAccessException;
 	
 	
 
