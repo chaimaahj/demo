fceb95728d674a5e6a830e9539b5f913015184eb
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-03-08 18:05:29 +0100

    Add files via upload

--- src/main/java/com/example/controllers/PanierController.java
+++ src/main/java/com/example/controllers/PanierController.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.example.model.LigneCmd;
+import com.example.model.Panier;
 import com.example.model.Produit;
 import com.example.repositories.PanierRepository;
 
 @Controller
-public class PanierController {
+public class PanierController  {
 
+	@Autowired
 	private PanierRepository pan;
 	HttpSession session;
 	private HashMap<Integer,LigneCmd> pdts = new HashMap<Integer,LigneCmd>();
@@ -23,11 +26,13 @@ public class PanierController {
 		{
 			LigneCmd l = pdts.get(p.getIdPdt());
 			l.setQte(l.getQte()+qte);
+			pan.save(new Panier(p.getIdPdt()));
 		}
 		else {
 			LigneCmd l= new LigneCmd();
 			l.setPdt(p);
 			l.setQte(qte);
+			pan.save(new Panier(p.getIdPdt()));
 		}
 		
 	}
